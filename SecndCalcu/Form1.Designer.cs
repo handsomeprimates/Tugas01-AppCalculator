0d6aaf4628a6cf85a64834a6e99390e1ce4b2f38
Bramasta <alan.br@students.amikom.ac.id>
2018-09-25 19:47:56 +0700

    Tugas 01

---
+++ SecndCalcu/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SecndCalcu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NamaComboBox = new System.Windows.Forms.ComboBox();
+            this.NamaText1 = new System.Windows.Forms.TextBox();
+            this.NamaText2 = new System.Windows.Forms.TextBox();
+            this.txtHasil = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operasi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nilai A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nilai B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Hasil";
+            // 
+            // NamaComboBox
+            // 
+            this.NamaComboBox.FormattingEnabled = true;
+            this.NamaComboBox.Items.AddRange(new object[] {
+            "Penambahan",
+            "Pengurangan",
+            "Perkalian",
+            "Pembagian"});
+            this.NamaComboBox.Location = new System.Drawing.Point(61, 27);
+            this.NamaComboBox.Name = "NamaComboBox";
+            this.NamaComboBox.Size = new System.Drawing.Size(121, 21);
+            this.NamaComboBox.TabIndex = 4;
+            // 
+            // NamaText1
+            // 
+            this.NamaText1.Location = new System.Drawing.Point(61, 55);
+            this.NamaText1.Name = "NamaText1";
+            this.NamaText1.Size = new System.Drawing.Size(121, 20);
+            this.NamaText1.TabIndex = 5;
+            // 
+            // NamaText2
+            // 
+            this.NamaText2.Location = new System.Drawing.Point(61, 82);
+            this.NamaText2.Name = "NamaText2";
+            this.NamaText2.Size = new System.Drawing.Size(121, 20);
+            this.NamaText2.TabIndex = 6;
+            // 
+            // txtHasil
+            // 
+            this.txtHasil.Location = new System.Drawing.Point(61, 109);
+            this.txtHasil.Name = "txtHasil";
+            this.txtHasil.Size = new System.Drawing.Size(121, 20);
+            this.txtHasil.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Hitung";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnHitung_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtHasil);
+            this.Controls.Add(this.NamaText2);
+            this.Controls.Add(this.NamaText1);
+            this.Controls.Add(this.NamaComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox NamaComboBox;
+        private System.Windows.Forms.TextBox NamaText1;
+        private System.Windows.Forms.TextBox NamaText2;
+        private System.Windows.Forms.TextBox txtHasil;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SecndCalcu/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecndCalcu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            Double nilaiA, nilaiB, hasil; // 3 variabel
+
+            if (NamaComboBox.SelectedIndex == 0)
+            {
+                nilaiA = Double.Parse(NamaText1.Text);
+                nilaiB = Double.Parse(NamaText2.Text);
+                hasil = nilaiA + nilaiB;
+                txtHasil.Text = hasil.ToString();
+            }
+            else if (NamaComboBox.SelectedIndex == 1)
+            {
+                nilaiA = Double.Parse(NamaText1.Text);
+                nilaiB = Double.Parse(NamaText2.Text);
+                hasil = nilaiA - nilaiB;
+                txtHasil.Text = hasil.ToString();
+            }
+            else if (NamaComboBox.SelectedIndex == 2)
+            {
+                nilaiA = Double.Parse(NamaText1.Text);
+                nilaiB = Double.Parse(NamaText2.Text);
+                hasil = nilaiA * nilaiB;
+                txtHasil.Text = hasil.ToString();
+            }
+            else if (NamaComboBox.SelectedIndex == 3)
+            {
+                nilaiA = Double.Parse(NamaText1.Text);
+                nilaiB = Double.Parse(NamaText2.Text);
+                hasil = nilaiA / nilaiB;
+                txtHasil.Text = hasil.ToString();
+            }
+        }
+    }
+}
